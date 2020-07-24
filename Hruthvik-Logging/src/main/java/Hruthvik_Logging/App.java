494030459eba239145f08f9e3175c25d8b4d2639
hruthvikpasumarthy <hruthvik.pasumarthy@gmail.com>
2020-07-24 16:57:21 +0530

    adding

---
+++ Hruthvik-Logging/src/main/java/Hruthvik_Logging/App.java
@@ -0,0 +1,13 @@
+package Hruthvik_Logging.Hruthvik_Logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Hruthvik-Logging/src/test/java/Hruthvik_Logging/Hruthvik_Logging/AppTest.java
@@ -0,0 +1,20 @@
+package Hruthvik_Logging.Hruthvik_Logging;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
